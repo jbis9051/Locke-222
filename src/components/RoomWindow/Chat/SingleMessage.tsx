bd4c87f6ec2e3e98b808d1160de0e967442e030a
Forrest Hopkins <fhopkins404@gmail.com>
2020-06-11 19:55:25 -0700

    Add message buttons, introduce CSS variables

--- src/components/RoomWindow/Chat/SingleMessage.tsx
+++ src/components/RoomWindow/Chat/SingleMessage.tsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useObserver } from 'mobx-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen } from '@fortawesome/free-solid-svg-icons/faPen';
+import { faStar } from '@fortawesome/free-solid-svg-icons/faStar';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons/faChevronDown';
 import Message from '../../../models/Message';
 import format from '../../../helpers/dateFormatter';
+import CurrentUserStore from '../../../stores/CurrentUserStore';
 import './SingleMessage.css';
 
 interface IMessageProps {
@@ -9,18 +14,37 @@ interface IMessageProps {
 }
 
 const SingleMessage: React.FunctionComponent<IMessageProps> = ({ message }) => {
-    const [hover, setHover] = useState(false);
+    const sentByCurrentUser = message.user.id === CurrentUserStore.user?.id;
 
     return useObserver(() => (
-        <div
-            onMouseEnter={() => setHover(true)}
-            onMouseLeave={() => setHover(false)}
-            className="single-message__container"
-        >
+        <div className="single-message__container">
             <div className="single-message__content">{message.content}</div>
-            {hover && (
+            <div className="single-message__info">
                 <div className="single-message__timestamp">{format(message.dateCreated)}</div>
-            )}
+                <div className="single-message__options">
+                    {sentByCurrentUser ? (
+                        <div
+                            className="single-message__button"
+                            onClick={() => console.log('Edit message')}
+                        >
+                            <FontAwesomeIcon icon={faPen} />
+                        </div>
+                    ) : (
+                        <div
+                            className="single-message__button"
+                            onClick={() => console.log('Star message')}
+                        >
+                            <FontAwesomeIcon icon={faStar} />
+                        </div>
+                    )}
+                    <div
+                        className="single-message__button"
+                        onClick={() => console.log('Open menu')}
+                    >
+                        <FontAwesomeIcon icon={faChevronDown} />
+                    </div>
+                </div>
+            </div>
         </div>
     ));
 };
